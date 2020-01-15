86b7d2e433cd80008485c5b6a3ed5880454e0fb9
nvarner <nathanmvarner@protonmail.com>
2020-01-15 10:02:13 -0500

    Add basic propaganda

--- src/rw1/HQRobot.java
+++ src/rw1/HQRobot.java
@@ -20,6 +20,7 @@ public strictfp class HQRobot extends Robot {
 
     @Override
     public void run() throws GameActionException {
+        propaganda();
 
         //Process nearby robots
         RobotInfo[] ri = nearbyRobots;
@@ -85,8 +86,48 @@ public strictfp class HQRobot extends Robot {
                 }
             }
         }
+    }
 
+    /**
+     * Teh devs have not been impartial. We must ensure that this travesty is not forgotten.
+     *
+     * Uses debug lines and dots to put "No bias" onto the field. Prints a longer message to the debug console.
+     */
+    private void propaganda() {
+        // N
+        rc.setIndicatorLine(new MapLocation(0, 0), new MapLocation(0, 4), 0, 0, 0);
+        rc.setIndicatorLine(new MapLocation(0, 4), new MapLocation(2, 0), 0, 0, 0);
+        rc.setIndicatorLine(new MapLocation(2, 0), new MapLocation(2, 4), 0, 0, 0);
 
+        // o
+        rc.setIndicatorLine(new MapLocation(3, 2), new MapLocation(5, 2), 0, 0, 0);
+        rc.setIndicatorLine(new MapLocation(5, 2), new MapLocation(5, 0), 0, 0, 0);
+        rc.setIndicatorLine(new MapLocation(5, 0), new MapLocation(3, 0), 0, 0, 0);
+        rc.setIndicatorLine(new MapLocation(3, 0), new MapLocation(3, 2), 0, 0, 0);
+
+        // Space
+
+        // b
+        rc.setIndicatorLine(new MapLocation(7, 0), new MapLocation(7, 4), 0, 0, 0);
+        rc.setIndicatorLine(new MapLocation(9, 2), new MapLocation(7, 2), 0, 0, 0);
+        rc.setIndicatorLine(new MapLocation(9, 2), new MapLocation(9, 0), 0, 0, 0);
+        rc.setIndicatorLine(new MapLocation(9, 0), new MapLocation(7, 0), 0, 0, 0);
+
+        // i
+        rc.setIndicatorLine(new MapLocation(10, 0), new MapLocation(10, 2), 0, 0, 0);
+        rc.setIndicatorDot(new MapLocation(10, 3), 0, 0, 0);
+
+        // a
+        rc.setIndicatorLine(new MapLocation(13, 1), new MapLocation(11, 1), 0, 0, 0);
+        rc.setIndicatorLine(new MapLocation(11, 1), new MapLocation(11, 0), 0, 0, 0);
+        rc.setIndicatorLine(new MapLocation(11, 0), new MapLocation(13, 0), 0, 0, 0);
+        rc.setIndicatorLine(new MapLocation(13, 0), new MapLocation(13, 2), 0, 0, 0);
+        rc.setIndicatorLine(new MapLocation(13, 2), new MapLocation(11, 2), 0, 0, 0);
+
+        // sz
+        rc.setIndicatorLine(new MapLocation(15, 2), new MapLocation(14, 2), 0, 0, 0);
+        rc.setIndicatorLine(new MapLocation(14, 2), new MapLocation(15, 0), 0, 0, 0);
+        rc.setIndicatorLine(new MapLocation(15, 0), new MapLocation(14, 0), 0, 0, 0);
     }
 
     @Override
